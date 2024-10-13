c79a70cbe07497873bb4eac51f50379cb6ca41c9
Lucas Castelano dos Santos <lucas.castelano@radixeng.com>
2024-10-13 20:38:32 -0300

    feat: add AppProvider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,12 +4,23 @@ import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from '@/styles/global'
 import theme from '@/styles/theme'
+import { AppProvider } from '@toolpad/core/nextjs'
+import branding from '@/config/branding'
+import navigation from '@/config/navigation'
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {children}
+      <AppProvider
+        branding={branding}
+        navigation={navigation}
+        session={null}
+        router={undefined}
+        theme={undefined}
+      >
+        <GlobalStyles />
+        {children}
+      </AppProvider>
     </ThemeProvider>
   )
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,9 @@
+import { DashboardLayout } from '@toolpad/core'
 import * as S from './styles'
 
-const Main = ({
-  title = 'React Avançado',
-  description = 'TypeScript, ReactJS, NextJS e Styled Components'
-}) => (
+const Main = () => (
   <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
-    />
-    <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela com código."
-    />
+    <DashboardLayout>Segment</DashboardLayout>
   </S.Wrapper>
 )
 
