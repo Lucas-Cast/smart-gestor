d3e991f7badc7fe1d32f09a151b4587a905e1215
Lucas Castelano dos Santos <lucas.castelano@radixeng.com>
2024-12-14 19:33:03 -0300

    feat: set main page dimensions

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,7 @@
-const Main = () => <div>Home page</div>
+const Main = () => (
+  <div className="w-full h-full min-h-screen pl-4 pr-16 pt-24 pb-16 text-primary-foreground">
+    wefefw wefwefwe wefwefwewfw wefwefwe fwefw efwf
+  </div>
+)
 
 export default Main
